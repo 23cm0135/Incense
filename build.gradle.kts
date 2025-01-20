665c9f3df0c118099553f69f2b68758d257af414
Shark0225 <158388180+Shark0225@users.noreply.github.com>
2025-01-20 14:20:14 +0900

    123
    123

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
 buildscript {
     repositories {
