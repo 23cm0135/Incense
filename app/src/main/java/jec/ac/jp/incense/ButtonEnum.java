03dab7ec92769c844686d3690d7569af118e595d
Shark0225 <158388180+Shark0225@users.noreply.github.com>
2025-02-19 14:40:28 +0900

    123
    213

--- app/src/main/java/jec/ac/jp/incense/ButtonEnum.java
+++ app/src/main/java/jec/ac/jp/incense/ButtonEnum.java
@@ -55,7 +55,35 @@ public enum ButtonEnum {
             R.drawable.senkou16, "https://www.kyukyodo-shop.co.jp/?pid=182693811"),
     BUTTON_17("id_17","お香 六種の薫物 黒方","平安時代から伝わる煉香の香りを、手軽に楽しめるスティックタイプのお香に仕立てました。\n" +
             "黒方は慶事の他、四季を通じて用いられ、玄妙な香りが特長のお香です。",
-            R.drawable.senkou17, "https://www.kyukyodo-shop.co.jp/?pid=136583045");
+            R.drawable.senkou17, "https://www.kyukyodo-shop.co.jp/?pid=136583045"),
+    BUTTON_18("id_18","お香 六種の薫物 梅花","平安時代から伝わる煉香の香りを、手軽に楽しめるスティックタイプのお香に仕立てました。\n" +
+            "梅花は梅の花を想わせる、春の香りです。",
+            R.drawable.senkou18, "https://www.kyukyodo-shop.co.jp/?pid=136583047"),
+    BUTTON_19("id_19","お香セット 香木の香り","伽羅・沈香・白檀 のセットです。",
+            R.drawable.senkou19, "https://www.kyukyodo-shop.co.jp/?pid=136583089"),
+    BUTTON_20("id_20","百楽香(ひゃくらくこう)木蓮","厳選された天然白檀と原料を調合した上質なお香です。\n" +
+            "お部屋焚き用はもちろん御仏前用としてもお使い頂けます。",
+            R.drawable.senkou20, "https://kuyukou.com/products/%E7%99%BE%E6%A5%BD%E9%A6%99-%E3%81%B2%E3%82%83%E3%81%8F%E3%82%89%E3%81%8F%E3%81%93%E3%81%86-%E6%9C%A8%E8%93%AE-%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF40%E6%9C%AC"),
+    BUTTON_21("id_21","かゆらぎ 白檀","ここちよい香りもその 『 ゆらぎ 』 のひとつかもしれません。\n" +
+            "「かゆらぎ」は、そんな自然の恵みを想起させる和風の香りをお香にしました。" +
+            "お部屋焚き用はもちろん御仏前用としてもお使い頂けます。",
+            R.drawable.senkou21, "https://kuyukou.com/products/kayuragi-byakudan"),
+    BUTTON_22("id_22","以花伝心　金木犀","秋の始まりを告げる、金木犀の香り",
+            R.drawable.senkou22, "https://kuyukou.com/products/%E4%BB%A5%E8%8A%B1%E4%BC%9D%E5%BF%83-%E9%87%91%E6%9C%A8%E7%8A%80-%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF20%E6%9C%AC%E5%85%A5"),
+    BUTTON_23("id_23","百楽香（ひゃくらくこう）楠","森の香気を浴びるような楠の香り",
+            R.drawable.senkou23, "https://kuyukou.com/products/%E7%99%BE%E6%A5%BD%E9%A6%99-%E3%81%B2%E3%82%83%E3%81%8F%E3%82%89%E3%81%8F%E3%81%93%E3%81%86-%E6%A5%A0-%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF40%E6%9C%AC"),
+    BUTTON_24("id_24","ESTEBAN（エステバン） ピュアリネン","エステバンのお香は400年以上にわたって育まれた日本伝統の薫香技術を用いて、日本で丁寧に作り上げられています。\n" +
+            "洗練された芳香で、落ち着きのある雰囲気をお楽しみいただけます。",
+            R.drawable.senkou24, "https://kuyukou.com/products/esteban-%E3%82%A8%E3%82%B9%E3%83%86%E3%83%90%E3%83%B3-%E3%83%94%E3%83%A5%E3%82%A2%E3%83%AA%E3%83%8D%E3%83%B3-%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF40%E6%9C%AC%E5%85%A5"),
+
+    BUTTON_25("id_25","山紫水明 嵯峨の朝霧　","『山紫水明』は、四季毎に色合いを変える西陣織のような山々に囲まれ、友禅流しの名残をとどめる澄んだ川を抱く京都。そんな自然の香りをお香に托したシリーズです。「嵯峨の朝霧」浅緑の竹林をしっとり濡らす嵯峨の朝霧をイメージした、緑色のお香です。シンプルでクリーンな、草原の香り。保存に適した桐箱入りです。",
+              R.drawable.senkou25, "https://kuyukou.com/products/sanshisuimei_saga"),
+    BUTTON_26("id_26","山紫水明　嵐山の月影","「嵐山の月影」梔子(くちなし)色にひんやりと川面に映る、嵐山の月影をイメージした灰色のお香です。エキゾチックで甘さを抑えた、大人っぽい香りです。保存に適した桐箱入りです。",
+              R.drawable.senkou26, "https://kuyukou.com/products/%E5%B1%B1%E7%B4%AB%E6%B0%B4%E6%98%8E-%E5%B5%90%E5%B1%B1%E3%81%AE%E6%9C%88%E5%BD%B1-%E6%A1%90%E7%AE%B1%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF30%E6%9C%AC%E5%85%A5"),
+    BUTTON_27("id_27","山紫水明　祇園の華","「祇園の華」今様色にはんなりとすれ違い香る、祇園の華をイメージした桃色のお香です。数種の花たちが醸し出す、情熱的な香り。保存に適した桐箱入りです",
+            R.drawable.senkou27, "https://kuyukou.com/products/%E5%B1%B1%E7%B4%AB%E6%B0%B4%E6%98%8E-%E7%A5%87%E5%9C%92%E3%81%AE%E8%8F%AF-%E6%A1%90%E7%AE%B1%E3%82%B9%E3%83%86%E3%82%A3%E3%83%83%E3%82%AF30%E6%9C%AC%E5%85%A5");
+
+
 
 
     private final String id;
